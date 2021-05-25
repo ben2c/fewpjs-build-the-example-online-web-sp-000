538fe00047459bf1ccd44bab29cfa5859adcbf28
Benjamin Cheng <bencheng200@gmail.com>
2021-05-25 20:12:34 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ const colorStats = {
   "" : "red"
 }
 
+const articleHearts
+
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
